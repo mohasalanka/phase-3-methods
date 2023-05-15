761938da3ae99605fc09fa0620e8a03f0226b3c3
Mohamed <mohasalanka2020@gmail.com>
2023-05-15 23:02:39 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+# methods.rb
+
+# greetProgrammer method
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+
+# greet method with an argument
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+# greetWithDefault method with optinal argument
+def greet_with_default(name= "programmer")
+    puts "Hello, #{name}!"
+end
+
+# add a methid with two arguments and return value
+def add(num1, num2)
+    num1 + num2
+end
+
+# halve method with one argument and return value, handling non-integer input
+def halve(number)
+    return nil unless number.is_a?(Integer)
+
+    number / 2
+end
